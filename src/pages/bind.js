de871c2ff87dd7bdcc43eba537a9733ec674d1ab
Manish Choudhary <manish.choudhary@technoforte.co.in>
2024-01-30 14:20:03 +0530

    Uncommented

--- src/pages/bind.js
+++ src/pages/bind.js
@@ -27,31 +27,31 @@ const Bind = () => {
 
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   const storedToken = localStorage.getItem(Constants.ACCESS_TOKEN);
+  useEffect(() => {
+    const storedToken = localStorage.getItem(Constants.ACCESS_TOKEN);
 
-  //   if (storedToken) {
-  //     setAccessToken(storedToken);
-  //     console.log("Access Token (Local Storage) : " + storedToken);
-  //     console.log("Time : " + new Date().toLocaleTimeString());
-  //   } else {
-  //     setLoading(true);
-  //     post_fetchAccessToken(authCode)
-  //       .then((token) => {
-  //         setAccessToken(token);
-  //         console.log("Access Token (API) : " + token);
-  //         console.log("Time : " + new Date().toLocaleTimeString());
-  //         localStorage.setItem(Constants.ACCESS_TOKEN, token);
-  //       })
-  //       .catch((error) => {
-  //         console.error("Error fetching user details:", error);
-  //         setError("Error fetching access token.");
-  //       })
-  //       .finally(() => {
-  //         setLoading(false);
-  //       });
-  //   }
-  // }, []);
+    if (storedToken) {
+      setAccessToken(storedToken);
+      console.log("Access Token (Local Storage) : " + storedToken);
+      console.log("Time : " + new Date().toLocaleTimeString());
+    } else {
+      setLoading(true);
+      post_fetchAccessToken(authCode)
+        .then((token) => {
+          setAccessToken(token);
+          console.log("Access Token (API) : " + token);
+          console.log("Time : " + new Date().toLocaleTimeString());
+          localStorage.setItem(Constants.ACCESS_TOKEN, token);
+        })
+        .catch((error) => {
+          console.error("Error fetching user details:", error);
+          setError("Error fetching access token.");
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, []);
 
   const generateSymmetricKey = () => {
     const secretKey = new Uint8Array(30);
@@ -107,7 +107,7 @@ const Bind = () => {
                 <img
                   className="object-contain fixed"
                   src="images/background_img.png"
-                  style={{ zIndex: -1, left: 200, bottom: 30}}
+                  style={{ zIndex: -1, left: 200, bottom: 30 }}
                 />
               </div>
             </div>
@@ -117,7 +117,13 @@ const Bind = () => {
                   <div className="row-span-5 w-96 self-start">
                     <div className="px-11 py-2">
                       {successMessage ? (
-                        <div className={`mt-5 text-center ${successMessage.includes(Constants.BIND_SUCCESS_MSG) ? 'bg-green-200 text-green-800 border border-green-400 p-2 rounded' : 'bg-red-200 text-red-800 border border-red-400 p-2 rounded'}`}>
+                        <div
+                          className={`mt-5 text-center ${
+                            successMessage.includes(Constants.BIND_SUCCESS_MSG)
+                              ? "bg-green-200 text-green-800 border border-green-400 p-2 rounded"
+                              : "bg-red-200 text-red-800 border border-red-400 p-2 rounded"
+                          }`}
+                        >
                           {successMessage}
                         </div>
                       ) : (
